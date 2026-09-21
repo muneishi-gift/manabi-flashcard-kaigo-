e232abf26db909f7a071d31339437b758447e6d8
muneishi-gift <mthd229@gmail.com>
2026-09-22 01:11:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js – 介護福祉士国家試験対策アプリ
 // 方式：ネットワーク優先（オンラインなら常に最新版を取得し、失敗時のみキャッシュを使用）
 
-const CACHE_NAME = 'kaigo-vf-v75';
+const CACHE_NAME = 'kaigo-vf-v76';
 
 // 最初から保存しておくファイル
 const ASSETS = [
@@ -49,6 +49,7 @@ const ASSETS = [
   './youten/data/youten-11a.js',
   './youten/data/youten-11b.js',
   './youten/data/youten-12.js',
+  './youten/data/youten-13.js',
 
 
   'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700;900&family=Noto+Sans+Thai:wght@400;700&family=Noto+Sans+Myanmar:wght@400;700&family=Noto+Sans+Devanagari:wght@400;700&family=Noto+Sans:wght@400;700&family=Poppins:wght@600;800&display=swap',
